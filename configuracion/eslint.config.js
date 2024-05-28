527eb83899fb424c30dcd4961ff69d135b6628a1
Noël Ruault <noelruault.engineer@gmail.com>
2024-05-28 14:58:22 +0200

    Añade stylistic/eslint-plugin-js y algunas reglas

--- configuracion/eslint.config.js
+++ configuracion/eslint.config.js
@@ -14,32 +14,30 @@ export default [
 			}
 		},
 		rules: {
-			"no-multi-spaces": ["error"],
-			"no-unused-vars":  "error",
-			"prefer-const":    [ "error", { "ignoreReadBeforeAssign": true }],
-			"indent":          [ "error", "tab"],
-			"linebreak-style": [ "error", "unix" ],
-			"quotes":          [ "error", "double" ],
-			"semi":            [ "error", "never" ],
-			"eqeqeq":          [ "error", "always" ], // enforce the use of strict equality operators
-			"strict":          [ "error", "global" ], // enforce the use of strict mode, which is enabled by default in ES6
+			"no-unused-vars": "error",
+			"prefer-const": ["error", { "ignoreReadBeforeAssign": true }],
+			"eqeqeq": ["error", "always"], // enforce the use of strict equality operators
+			"strict": ["error", "global"], // enforce the use of strict mode, which is enabled by default in ES6
 
 			// (Optional) Stylistic rules
-			"space-infix-ops":     [ "error", { "int32Hint": false } ],
-			"space-before-blocks": [ "error",
-				{
-					"functions": "always",
-					"keywords":  "always",
-					"classes":   "never"
-				}
+			"@stylistic/js/no-multi-spaces": ["error"],
+			"@stylistic/js/indent": ["error", "tab"],
+			"@stylistic/js/linebreak-style": ["error", "unix"],
+			"@stylistic/js/quotes": ["error", "double"],
+			"@stylistic/js/semi": ["error", "never"],
+			"@stylistic/js/space-infix-ops": ["error", { "int32Hint": false }],
+			"@stylistic/js/space-before-blocks": ["error", {
+				"functions": "always",
+				"keywords": "always",
+				"classes": "never"
+			}
 			],
-			"key-spacing": [ "error",
-				{
-					"beforeColon": false,
-					"afterColon":  true,
-					"mode":        "minimum",
-					"align":       "value"
-				}
+			"@stylistic/js/key-spacing": ["error", {
+				"beforeColon": false,
+				"afterColon": true,
+				"mode": "minimum",
+				"align": "value"
+			}
 			],
 		},
 	}
